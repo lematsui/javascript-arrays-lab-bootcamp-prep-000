d74ec1fbb882795dd56fd500e9ab544a9b5ec561
Ariel Matsui-LeRoy <arielmatsuileroy@gmail.com>
2018-08-27 15:48:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,11 +6,19 @@ function destructivelyAppendKitten(name) {
   kittens.push(name);
 }
 
-function destructivelyPrependKitten
+function destructivelyPrependKitten(name) {
   kittens.unshift(name);
 }
 
+function destructivelyRemoveLastKitten(name) {
+  kittens.pop(name);
+}
 
+function destructivelyRemoveFirstKitten(name) {
+  kittens.shift(name);
+}
+
+function 
 
 
 
